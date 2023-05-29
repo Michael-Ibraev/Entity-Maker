c12b9aabb6e47e0cfcde842b87d541362cf9024e
Michael-Ibraev <mikhail12.876@gmail.com>
2023-05-29 20:57:53 +0300

    Class, columns, pk and type generation added

--- src/main/java/com/example/entitymaker/EntityGenerator.java
+++ src/main/java/com/example/entitymaker/EntityGenerator.java
@@ -25,6 +25,7 @@ public class EntityGenerator implements Initializable {
 
     public static Map<String, String[][]> tables = new HashMap<String, String[][]>();
     public static Map<String, String[]> links = new HashMap<String, String[]>();
+    String[] tablesKeys;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -37,6 +38,9 @@ public class EntityGenerator implements Initializable {
         for(int i = 0; i < TableEditor.nameMap.size(); i++){
             tables.put(TableEditor.nameMap.get(nameKeys[i]), TableEditor.columnMap.get(nameKeys[i]));
         }
+
+        tablesKeys = tables.keySet().toArray(new String[0]);
+
 
         for(int i = 0; i < TableEditor.linkMap.size(); i++){
             String[] buffer = TableEditor.linkMap.get(linkKeys[i]);
@@ -55,14 +59,73 @@ public class EntityGenerator implements Initializable {
     }
 
     public void entityGenerate(ActionEvent actionEvent) throws IOException {
-        fileName = "qwerty";
-        File file = new File(textField.getText() + fileName + ".java");
-        file.createNewFile();
-        FileWriter fileWriter = new FileWriter(file);
-        fileWriter.write("package models;\n\n" +
-                "import javax.persistence.*;\n" +
-                "import java.util.ArrayList;\n" +
-                "import java.util.List;");
-        fileWriter.close();
+//        for(int i = 0; i < tables.size(); i++){
+//            script += "CREATE TABLE IF NOT EXISTS `" +  tablesKeys[i] +"`(\n";
+//            String[][] value = tables.get(tablesKeys[i]);
+//            for(int k = 0; k < value.length; k++){
+//                for(int j = 0; j < 5; j++ ){
+//                    if(j == 0){
+//                        script += "`"+ value[k][j] + "` ";
+//                    } else if (j == 1) {
+//                        script += value[k][j];
+//                    } else if (value[k][j].equals("PK")) {
+//                        script += " PRIMARY KEY";
+//                    } else if (value[k][j].equals("NN")){
+//                        script += " NOT NULL";
+//                    } else if (value[k][j].equals("AI")){
+//                        script += " AUTO_INCREMENT";
+//                    }
+//                }
+//                if(k == value.length-1){
+//                    script += ");\n\n";
+//                } else {
+//                    script += ",\n";
+//                }
+//            }
+//        }
+        for(int i = 0; i < tables.size(); i++) {
+            fileName = tablesKeys[i];
+            File file = new File(textField.getText() + fileName + ".java");
+            file.createNewFile();
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write("package models;\n\n" +
+                    "import javax.persistence.*;\n" +
+                    "import java.util.*;\n\n" +
+                    "@Entity\n" +
+                    "@Table(name = " + "\"" + fileName + "\")\n" +
+                    "public class " + fileName + " {\n");
+            String[][] value = tables.get(tablesKeys[i]);
+            for(int k = 0; k < value.length; k++){
+                String columnName = "";
+                String columnType = "";
+                boolean pk = false;
+                for(int j = 0; j < 5; j++){
+                    if(j == 0){
+                        columnName = value[k][j];
+                    } else if(j == 1){
+                        if(value[k][j].equals("INT")){
+                            columnType = "int";
+                        } else if(value[k][j].contains("VARCHAR") || value[k][j].contains("TEXT")){
+                            columnType = "String";
+                        } else if (value[k][j].contains("REAL") || value[k][j].contains("DECIMAL")){
+                            columnType = "float";
+                        }
+                    } else if (j == 2) {
+                        if(value[k][j].equals("PK")){
+                            pk = true;
+                        } else {
+                            pk = false;
+                        }
+                    }
+                }
+                if(pk){
+                    fileWriter.write("\t@Id\n");
+                }
+                fileWriter.write("\t@Column(name = \"" + columnName + "\")\n" +
+                        "\tprivate " + columnType + " " + columnName + ";\n\n");
+            }
+            fileWriter.write("\n}");
+            fileWriter.close();
+        }
     }
 }
